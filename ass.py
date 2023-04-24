0bc9d1617a5b6e1fbbe096ffd211da3d4a9d6191
Saksham Singh <saksham5904@gmail.com>
2023-04-24 21:13:14 +0530

    type A instructions done

--- ass.py
+++ ass.py
@@ -22,6 +22,15 @@ op_codes = {
             'hlt':('11010','F')
             }
 
+op_codes_A = {
+              'add':'00000',
+              'sub':'00001',
+              'mul':'00110',
+              'xor':'01010',
+              'or':'01011',
+              'and':'01100'
+             }
+
 regs = {
         'R0':'000',
         'R1':'001',
@@ -32,3 +41,18 @@ regs = {
         'R6':'110'
         }
 
+f_input =  open("read.txt",'r+')
+f_output = open("write.txt",'a+')
+
+f_output.seek(0)
+f_output.truncate()
+
+for line in f_input:
+    line = line.split()
+    
+    #type A, error handling left
+    if line[0] in op_codes_A:
+        string = ''
+        string += op_codes_A[line[0]]+'00'+(regs[line[1]]+regs[line[2]]+regs[line[3]])
+        string = string[0:4]+'_'+string[4:8]+'_'+string[8:12]+'_'+string[12:16]
+        f_output.write(string+"\n")
