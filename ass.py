32e55f9fcad5423958eca45e652175a4252d5807
Shagun Yadav <shagun22466@iiitd.ac.in>
2023-04-29 23:00:05 +0530

    hlt cases accounted for and the code works without recursion

--- ass.py
+++ ass.py
@@ -100,6 +100,73 @@ for i in range(len(input_list)):
 vars = {}
 var_index = prog_count
 
+def label_read(line):
+    global vars
+    global var_index
+
+    #type A, error handling left
+    if line[0] in op_codes_A:
+        string = op_codes_A[line[0]] + '00' + (regs[line[1]] + regs[line[2]] + regs[line[3]])
+        string = string[0:4] + '_' + string[4:8] + '_' + string[8:12] + '_' + string[12:16] + " Type A" + " " + line[0] + " " + line[1] + " " + line[2] + " " + line[3]
+        f_output.write(string + "\n")
+
+
+    #type B
+    elif (line[0] in op_codes_B) and (line[2][0] == "$") : # this sorts the mov problem by checking reg or $imm
+        binary = bin(int(line[2][1:])).replace("0b", "")        
+                                                    #I have ignored the case where they give negative number as input.
+        if len(binary) > 7:
+            string = "overflow error" + " Type B" + " " + line[0] + " " + line[1] + " " + line[2]
+            f_output.write(string + "\n")
+        else:
+            if len(binary) == 7:
+                pass  
+            elif len(binary) < 7:
+                binary = (7 - len(binary))*"0" + binary        
+        
+            string = op_codes_B[line[0]] + '0'+ (regs[line[1]]) + binary
+            string = string[0:4] + '_' + string[4:8] + '_' + string[8:12] + '_' + string[12:16] + " Type B" + " " + line[0] + " " + line[1] + " " + line[2]
+            f_output.write(string + "\n")
+
+
+    #type C
+    elif line[0] in op_codes_C:
+        string = op_codes_C[line[0]] + '0'*5 + (regs[line[1]] + regs[line[2]])
+        string = string[0:4] + '_' + string[4:8] + '_' + string[8:12] + '_' + string[12:16] + " Type C" + " " + line[0] + " " + line[1] + " " + line[2]
+        f_output.write(string + "\n")
+
+
+    #type D, needs to be tested
+    elif line[0] == 'var':
+        #need to convert the entire input into a 2d list, tabhi vars can be indexed and accessed easily
+        vars.update({line[1]:bin(var_index)[2:]})
+        if len(vars[line[1]]) == 7:
+            pass  
+        elif len(vars[line[1]]) < 7:
+            vars[line[1]] = (7 - len(vars[line[1]]))*"0" + vars[line[1]]
+        var_index += 1
+    
+    elif line[0] in op_codes_D:
+        if line[2] in vars:
+            string = op_codes_D[line[0]] + '0' + regs[line[1]] + vars[line[2]]
+            string = string[0:4] + '_' + string[4:8] + '_' + string[8:12] + '_' + string[12:16] + " Type D" + " " + line[0] + " " + line[1] + " " + line[2]
+            f_output.write(string + "\n")
+
+
+    #type E, handle the error if label was never initialised
+    elif line[0] in op_codes_E:
+        mem_addr = labels[line[1]]
+        string = op_codes_E[line[0]] + '0'*(4 + 7 - len(mem_addr)) + mem_addr
+        string = string[0:4] + '_' + string[4:8] + '_' + string[8:12] + '_' + string[12:16] + " Type E" + " " + line[0] + " " + line[1]
+        f_output.write(string + '\n')
+
+
+    #type F, no need to test i hope
+    elif line[0] == 'hlt':
+        string = op_codes_F[line[0]] + '0'*11
+        string = string[0:4] + '_' + string[4:8] + '_' + string[8:12] + '_' + string[12:16] + " Type F" + " " + line[0]
+        f_output.write(string + '\n')
+   
 def output_func(line):
     global vars
     global var_index
@@ -169,26 +236,23 @@ def output_func(line):
 
     #labels
     elif line[0][-1] == ':':
-        output_func(line[1:])
+        label_read(line[1:])
 
-    # if none of the opcodes match
-    else:
-        assert 0==1,f'Syntax Error in the opcode for line {line}\n'
-       
 
-if 'hlt' in input_list[len(input_list)-1]:#the only condition under which the program proceeds further, hlt at the end
+
+if 'hlt' in input_list[len(input_list)-1]: # the only condition under which the program proceeds further, hlt at the end
         halt_flag=True
 else:
     for i in range(len(input_list)-1):
         if 'hlt' in input_list[i]:
-            assert halt_flag==True,"'hlt' not being used as the last instruction"#throws off execution of the program, hlt not at the end
+            assert halt_flag==True,"'hlt' not being used as the last instruction" # throws off execution of the program, hlt not at the end
 
 
 if halt_flag:
     for line in input_list:
         output_func(line)
 else:
-    assert halt_flag==True,"something something"#put a suitable error in here
+    assert halt_flag==True,"'hlt' statement is absent" # put a suitable error in here
 
 
 
