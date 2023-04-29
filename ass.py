4b094f63178a2d0d5c0bf434b24764db9f3992b2
Shagun Yadav <shagun22466@iiitd.ac.in>
2023-04-29 23:23:04 +0530

    type A fixed in both functions

--- ass.py
+++ ass.py
@@ -104,8 +104,10 @@ def label_read(line):
     global vars
     global var_index
 
-    #type A, error handling left
+    #type A, error handling done :)
     if line[0] in op_codes_A:
+        if line[1] not in regs or line[2] not in regs or line[3] not in regs:
+            assert 0==1, "Invalid register name or some typo in register name"
         string = op_codes_A[line[0]] + '00' + (regs[line[1]] + regs[line[2]] + regs[line[3]])
         string = string[0:4] + '_' + string[4:8] + '_' + string[8:12] + '_' + string[12:16] + " Type A" + " " + line[0] + " " + line[1] + " " + line[2] + " " + line[3]
         f_output.write(string + "\n")
@@ -171,7 +173,7 @@ def output_func(line):
     global vars
     global var_index
 
-    #type A, register error handling done
+    #type A, register error handling
     if line[0] in op_codes_A:
 
         if line[1] not in regs or line[2] not in regs or line[3] not in regs:
